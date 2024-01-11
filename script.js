4355fc5474df939c49812cd5e37bf7661ab148f0
NicoDiLullo <65972630+NicoDiLullo@users.noreply.github.com>
2024-01-10 22:09:41 -0800

    add initial files

---
+++ script.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById("contactModal");
+var btn = document.getElementById("contactBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll(".carousel img");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
